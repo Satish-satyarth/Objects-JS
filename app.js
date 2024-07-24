32eb814e41fbaf1c87a0afa537c7f8f42b8cf888
satish-satyarth <satishsatyarth2002@gmail.com>
2024-07-25 01:22:40 +0530

    latest updates

--- app.js
+++ app.js
@@ -30,4 +30,72 @@ userNme.email_id = "satishyadav@gmail.com"
 userNme.greeting = function(){
     console.log(`hello satish , ${this.college}`);
 }
-console.log(userNme.greeting());
+// console.log(userNme.greeting());
+
+const tinder = new Object();    // Singleton object
+
+const tinder2 = {}
+
+tinder2.name1 = "satish"
+tinder2.name2 = "sagar"
+tinder2.name3 = "shiva"
+tinder2.name4 = "sikku"
+// console.log(tinder2);
+
+let obj2 = {1:"a", 2:"b",3:"c"}
+let obj3 = {4:"a", 5:"b", 6:"c"}
+
+/* const obj4= Object.assign({}, obj2, obj3)    -> Object.assign id used to combin two object literals 
+in one like array.*/
+// console.log(obj4)
+
+let obj5 = {...obj2,...obj3}  
+// console.log(obj5);           -> (...)Spread method is used for combine two object in one like array
+
+let marks = {
+    Science : {
+        phy : 69,
+        chem : 98,
+        bio : 96,
+    },
+
+    Socialscience : {
+        history:{
+            ancient_history: 59,
+            medieval_history:65,
+            modern_history:89,
+        },
+
+        civic:59,
+        poltics:89,
+        geography:67,
+    },
+
+    math : 99,
+    english :100,
+}       
+
+// console.log(marks.Socialscience);
+
+
+const arr = [
+    {
+        id:"s@gmail.com",
+        pass:"1233"
+    },
+
+    {
+        id:"sk@gmail.com",
+        pass:"12315",
+    }, 
+
+    {
+
+    }
+]
+
+console.log(arr[1].id);
+
+console.log(Object.keys(marks));     //The Object.keys() method returns an array with the keys of an object.
+console.log(Object.values(marks));
+console.log(Object.entries(arr));
